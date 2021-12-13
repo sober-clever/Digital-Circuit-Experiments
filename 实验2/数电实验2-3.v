d32b99a013bb0befeda1831d5f2d3c65a34330ac
sober-clever <75778480+sober-clever@users.noreply.github.com>
2021-12-13 19:56:53 +0800

    Delete 实验2 directory

--- 实验2/数电实验2-3.v
+++
@@ -1,8 +0,0 @@
-module decoder (
-    input i7,i6,i5,i4,i3,i2,i1,i0,
-    output y2,y1,y0
-);
-    assign y2 = i7|(~i7&i6)|(~i7&~i6&i5)|(~i7&~i6&~i5&i4);
-    assign y1 = i7|(~i7&i6)|(~i7&~i6&~i5&~i4&i3)|(~i7&~i6&~i5&~i4&~i3&i2);
-    assign y0 = i7|(~i7&~i6&i5)|(~i7&~i6&~i5&~i4&i3)|(~i7&~i6&~i5&~i4&~i3&~i2&i1);
-endmodule
